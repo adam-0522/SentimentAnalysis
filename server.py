2b1c8d757841387608235ed3d56d3c0f2964d03e
“AdamC” <415744211@qq.com>
2023-08-25 14:11:02 -0400

    server.py static code analyzed
    Signed-off-by: “AdamC” <“415744211@qq.com”>

--- server.py
+++ server.py
@@ -19,8 +19,7 @@ def sent_analyzer():
     score = response['score']
     if label is None:
         return "Invalid input ! Try again."
-    else:
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
@@ -30,5 +29,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000'''
     app.run(host="0.0.0.0", port = 5000)
