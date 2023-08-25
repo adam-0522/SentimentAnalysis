c96049410d93325332c14dbc813182e141318a82
“AdamC” <415744211@qq.com>
2023-08-25 14:05:57 -0400

    error handling: invalid input
    Signed-off-by: “AdamC” <“415744211@qq.com”>

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -7,6 +7,10 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
     formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
-    return {'label':label, 'score':score}
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+    return {'label': label, 'score': score}
